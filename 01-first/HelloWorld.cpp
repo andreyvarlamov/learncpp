5c8ba6c20edd1070054bd58e1c325c609f70b83c
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-03 21:19:17 -0800

    More g++ things

--- 01-first/HelloWorld.cpp
+++ 01-first/HelloWorld.cpp
@@ -7,3 +7,14 @@ int main()
 }
 
 // g++ is equivalent to gcc -xc++ -lstdc++ -shared-libgcc
+// To disable compiler extensions, add -pedantic-errors to compile command
+// To show more warnings:
+// -Wall: enable all common compiler warnings
+// -Weffc++: effective-c++ warnings (Scott Meyers)
+// -Wextra: extra compiler warnings
+// -Wsign-conversion
+// -Werror: treat errors as warnings
+// -std=c++11, -std=c++14, -std=c++17, -std=c++20 to support that language standard
+// For GCC 8 or 9, use -std=c++2a for c++20
+//
+// In the end: g++ -o HelloWorld.out HelloWorld.cpp -std=c++2a -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror
