d500e17a7c2d15bd65365eb67e88601298f97d61
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-04 16:05:09 -0800

    Tutorial > 1.5 - Notes for iostream basics

--- 01-first/HelloWorld.cpp
+++ 01-first/HelloWorld.cpp
@@ -5,7 +5,7 @@ int main()
     // int x; // Variable definition
     // x = 5; // Assign value - copy assignment (copy RHS of the = operator to the variable on the LHS)
 
-    // Variable initialization
+    // 1.4 - Variable initialization
     // No initializer - default initialization - in most cases, leaves a variable with an indeterminate value
     // int a;
     // Copy initialization
@@ -32,17 +32,51 @@ int main()
     // Empty braces -> value initialization; in most cases will initialize to zero (or
     // empty, if that's more appropriate for a given type).
     // If initializing to zero -> zero initialization
-    // int f {};
+    // int f { };
     // Use int x { 0 }; (explicit) if actually using that value
-    // Use int x {}; if the value is temporary and to be replaced
+    // Use int x { }; if the value is temporary and to be replaced
     // (Convention)
     // Best practive is to always initialize variables upon creation (unless perf reasons)
 
     // If variable unused -> warning (and if -Werror -> error)
     // In C++17 - [[maybe_unused]] attribute
-    [[maybe_unused]] int x { 5 };
+    [[maybe_unused]] int unused { 5 };
 
-    std::cout << "Hello, world!";
+    // 1.5 - Intro to iostream
+    // std::cout: Send data to the console to print as text (cout = "character output")
+    // <<: Insertion Operator
+    // E.g. std::cout << "Hello, world!";
+    // Can also print numbers: std::cout << 4;
+    // Can also print out the value of variables: int x { 5 }; std::cout << x;
+    // Insertion operator can be used multiple times to concatenate
+    // E.g. std::cout << "Hello" << " world!";
+    // Also: int x { 5 }; std::cout << "x is equal to: " << x;
+    // No end line with the insertion operator -> std::endl
+    // e.g. std::cout << "Hi!" << std::endl; std::cout << "blah" << std::endl;
+
+    // std::endl vs '\n'
+    // std::endl does two jobs: moves cursor to next line; makes sure that cached output shows
+    // up on the screen immediately (= flusing the output).
+    // When writing text to the consolue using std::cout, it often flushes the output anyway
+    // (and if it doesn't, it usually doesn't matter), so having std::endl perform a flush
+    // is rarely important.
+    // '\n' is typically preferred - more efficient (no flush requested)
+    std::cout << "Hello, world!\n";
+
+    // std::cin - "character input"; >> - "extraction operator"
+    // std::cout << "Enter a num: "; int x { }; std::cin >> x; std::cout << "You entered " << x << '\n';
+    // Note that you don't need to use '\n' when accepting input, as the user will need to
+    // press the ENTER key to have their input accepted, and this will move the cursor
+    // to the next line of the console
+    std::cout << "Enter two numbers separated by a space: ";
+    int x { };
+    int y { };
+    std::cin >> x >> y;
+    std::cout << "You entered " << x << " and " << y << '\n';
+
+    // iostream does not provide a way to accept keyboard input without the user having to
+    // press ENTER. For console, use pdcurses, FXTUI, or cpp-terminal
+
     return 0;
 }
 
