429742c4fd2fdd4b3f42971042b1c9b70939fd46
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-06 20:40:34 -0800

    Tutorial > 3.1 - Notes on types of errors

---
+++ 03-debugging/main.cpp
@@ -0,0 +1,9 @@
+// 3.1 - Errors
+// Syntax errors - statement invalid according to the grammar rules of C++
+// Semantiv errors - syntactically valid, but does not do what's intended
+// --> Debugging
+
+int main()
+{
+    return 0;
+}
