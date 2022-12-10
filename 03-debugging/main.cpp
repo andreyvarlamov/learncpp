cc3898177035677a5d462829c61f579ed2f12b94
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-09 20:13:45 -0800

    Tutorial > 3.10 - Notes for debugging

--- 03-debugging/main.cpp
+++ 03-debugging/main.cpp
@@ -3,7 +3,31 @@
 // Semantiv errors - syntactically valid, but does not do what's intended
 // --> Debugging
 
+#include <iostream>
+
+void print_value(int value)
+{
+    int x = 10;
+    ++x;
+    std::cout << value << " " << x << '\n';
+}
+
 int main()
 {
+    print_value(5);
     return 0;
 }
+
+// 3.4 - Basic Debugging Tactics
+// Use std::cerr, instead of std::cout for debugging messages (unbuffered)
+
+// gdb legend
+// break main or break 11
+// start
+// continue
+// step
+// next
+// finish (step over)
+// info local, info args, info variables
+// explore x
+// backtrace
