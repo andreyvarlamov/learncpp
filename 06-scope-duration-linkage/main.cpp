7ccbc06628ba477cd00c41cfdcfcc42be1def875
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-10 21:05:04 -0800

    Tutorial > 6.7 - External linkage and variable forward declaration

--- 06-scope-duration-linkage/main.cpp
+++ 06-scope-duration-linkage/main.cpp
@@ -351,3 +351,86 @@ int main()
 // Functions default to external linkage, but can be set to internal linkage via the
 // static keyword
 // E.g. static int add(int x, int y) { ... }
+
+
+
+// 6.7 - External linkage and variable forward declarations
+// An identifier with external linkage can be seen and used both from the file in which
+// it is defined, and from other code files (via a forward declration). In this sense,
+// identifiers with external linkage are truly "global" in that they can be used anywhere
+// in your program.
+
+// Functions have external linkage by default
+// In order to call a function defined in another file, you must place a forward
+// declaration for the function in any other files wishing to use the function. The
+// forward declaration tells the compiler about the existence of the function, and the
+// linker connects the function calls to the actual function definition.
+
+// Global variables with external linkage
+// Sometimes called external variables
+// extern keyword
+// E.g. extern const int g_z { 3 };
+// Non-const global variables are external by default (if used, the extern keyword will
+// be ignored).
+
+// Variable forward declarations via the extern keyword
+// To actually use an external global variable that has been defined in another file, you
+// also must place a forward declaration for the global variable in any other files wishing
+// to use the variable. For variables, creating a forward declaration is also done via
+// the extern keyword (with no initialization value).
+
+// Note that the extern keyword has different meanings in different contexts. In some contexts,
+// extern means "give this variable external linkage". In other contexts, extern means
+// "this is a forward declaration for an external variable that is defined somewhere else".
+// Warning: if you want to define an uninitialized non-const global variable, do not use
+// the extern keyword, otherwise C++ will think you're trying to make a forward declaration
+// for the variable.
+// Warning: although constexpr variables can be given external linkage via the extern
+// keyword, they can not be forward declared, so there is no value in giving them external
+// linkage.
+// This is because the compiler needs to know the value of the constexpr variable (at
+// compile time). If that value is defined in some other file, the compiler has no
+// visibility on what value was defined in that other file.
+// Note that function forward declarations don't need the extern keyword. The compiler is
+// able to tell whether you're defining a new function or making a forward declaration
+// based on whether you supply a function body or not.
+// Variable forward declarations do need the extern keyword to help differentiate variables
+// definitions from variable forward declarations (they look otherwise identical).
+//// non-constant
+//int g_x; // variable defintion (can have initializer if desired)
+//extern int g_x; // forward declaration (no initializer)
+//
+//// constant
+//extern const int g_y { 1 }; // variable definition (const requires initializers)
+//extern const int g_y;  // forward declaration (no initializer)
+
+// File scope vs global scope
+// The terms "file scope" and "global scope" tend to cause confusion. Partly due to how
+// they are used informally.
+// Technically, in C++, all global variables have "file scope", and the linkage property
+// controls whether they can be used in other files or not.
+// E.g.
+// global.cpp:
+//int g_x { 2 }; // external linkage by default
+//// g_x goes out of scope here
+
+// main.cpp:
+//extern int g_x; // forward declaration for g_x -- g_x can be used beyond this point in this file
+//
+//int main()
+//{
+//    std::cout << g_x << '\n'; // should print 2
+//
+//    return 0;
+//}
+// the forward declaration for g_x goes out of scope here
+
+// Variable g_x has file scope within global.cpp -- it can be used from the point of
+// defintion to the end of the file, but it can not be directly seen outside of global.cpp
+// Inside main.cpp, the forward declaration of g_x also has file scope -- it can be used
+// from the point of declaration to the end of the file.
+
+// However, informally, the term "file scope" is more often applied to global
+// variables with internal linkage, and "global scope" to global variables with external
+// linkage (since they can be used across the whole program, with the appropriate forward
+// declarations).
