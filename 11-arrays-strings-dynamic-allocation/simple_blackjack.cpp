47af5aee08b2235f79945fd1e564a64ddf5c39f3
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-16 21:00:27 -0800

    Quiz > 11.x - Q7 - Simple blackjack game

--- 11-arrays-strings-dynamic-allocation/simple_blackjack.cpp
+++ 11-arrays-strings-dynamic-allocation/simple_blackjack.cpp
@@ -1,4 +1,4 @@
-// 11.x - Q6
+// 11.x - Q6, Q7
 #include <algorithm>
 #include <array>
 #include <cassert>
@@ -42,54 +42,6 @@ struct Card
 };
 
 using Deck = std::array<Card, 52>;
-
-Deck createDeck()
-{
-    Deck deck { };
-
-    size_t index { 0 };
-    for (int i { 0 }; i < static_cast<int>(Suit::max_suits); ++i)
-    {
-        for (int j { 0 }; j < static_cast<int>(Rank::max_ranks); ++j)
-        {
-            deck[index].suit = static_cast<Suit>(i);
-            deck[index].rank = static_cast<Rank>(j);
-
-            ++index;
-        }
-    }
-
-    return deck;
-}
-
-void shuffleDeck(Deck& deck)
-{
-    static std::mt19937 mt { static_cast<std::mt19937::result_type>(std::time(nullptr)) };
-    std::shuffle(deck.begin(), deck.end(), mt);
-}
-
-int getCardValue(const Card& card)
-{
-    switch (card.rank)
-    {
-    case Rank::two:   return 2;
-    case Rank::three: return 3;
-    case Rank::four:  return 4;
-    case Rank::five:  return 5;
-    case Rank::six:   return 6;
-    case Rank::seven: return 7;
-    case Rank::eight: return 8;
-    case Rank::nine:  return 9;
-    case Rank::ten:   return 10;
-    case Rank::jack:  return 10;
-    case Rank::queen: return 10;
-    case Rank::king:  return 10;
-    case Rank::ace:   return 11;
-    default:          
-        assert(false && "should never happen");
-        return 0;
-    }
-}
 
 void printCard(const Card& card)
 {
@@ -175,18 +127,150 @@ void printDeck(const Deck& deck)
     std::cout << '\n';
 }
 
+Deck createDeck()
+{
+    Deck deck { };
+
+    size_t index { 0 };
+    for (int i { 0 }; i < static_cast<int>(Suit::max_suits); ++i)
+    {
+        for (int j { 0 }; j < static_cast<int>(Rank::max_ranks); ++j)
+        {
+            deck[index].suit = static_cast<Suit>(i);
+            deck[index].rank = static_cast<Rank>(j);
+
+            ++index;
+        }
+    }
+
+    return deck;
+}
+
+void shuffleDeck(Deck& deck)
+{
+    static std::mt19937 mt { static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+    std::shuffle(deck.begin(), deck.end(), mt);
+}
+
+int getCardValue(const Card& card)
+{
+    switch (card.rank)
+    {
+    case Rank::two:   return 2;
+    case Rank::three: return 3;
+    case Rank::four:  return 4;
+    case Rank::five:  return 5;
+    case Rank::six:   return 6;
+    case Rank::seven: return 7;
+    case Rank::eight: return 8;
+    case Rank::nine:  return 9;
+    case Rank::ten:   return 10;
+    case Rank::jack:  return 10;
+    case Rank::queen: return 10;
+    case Rank::king:  return 10;
+    case Rank::ace:   return 11;
+    default:
+        assert(false && "should never happen");
+        return 0;
+    }
+}
+
+bool playBlackjack(const Deck& deck)
+{
+    int playerScore { };
+    int dealerScore { };
+
+    auto currentCard { deck.begin() }; // returns iterator (I think it overloads * and ++)
+    // Dealer gets one card
+    dealerScore += getCardValue(*currentCard);
+    ++currentCard;
+    std::cout << "Dealer draws one card. Score: " << dealerScore << '\n';
+
+    // Player gets two cards
+    playerScore += getCardValue(*currentCard);
+    ++currentCard;
+    playerScore += getCardValue(*currentCard);
+    ++currentCard;
+
+    std::cout << "Player draws two cards. Score: " << playerScore << '\n';
+
+    // Player's turn: hit until stand; if > 21 - bust
+    while (true)
+    {
+        char hitChar { };
+        std::cout << "Hit(y/n)? ";
+        std::cin >> hitChar; // TODO: Do this safely
+
+        if (hitChar != 'y')
+        {
+            break;
+        }
+
+        playerScore += getCardValue(*currentCard);
+        ++currentCard;
+
+        std::cout << "You hit. Score: " << playerScore << '\n';
+
+        if (playerScore > 21)
+        {
+            std::cout << "You lose!\n";
+            return false;
+        }
+    }
+
+    // Dealer's turn: draw until score > 17; if > 21 - bust
+    while (dealerScore <= 17)
+    {
+        dealerScore += getCardValue(*currentCard);
+        ++currentCard;
+
+        std::cout << "Dealer draws. Score: " << dealerScore << '\n';
+
+        if (dealerScore > 21)
+        {
+            std::cout << "Dealer loses. You win!\n";
+            return true;
+        }
+    }
+
+    // If no bust yet: compare scores. If tie: dealer wins
+    if (playerScore > dealerScore)
+    {
+        std::cout << "You win!\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "You lose!\n";
+        return false;
+    }
+
+}
+
 int main()
 {
     Deck deck { createDeck() };
-    std::cout << "Sorted deck:\n";
-    printDeck(deck);
+    if (false) // No cheating!
+    {
+        std::cout << "Sorted deck:\n";
+        printDeck(deck);
 
-    std::cout << '\n';
+        std::cout << '\n';
 
-    shuffleDeck(deck);
-    std::cout << "Shuffled deck:\n";
-    printDeck(deck);
+        shuffleDeck(deck);
+        std::cout << "Shuffled deck:\n";
+        printDeck(deck);
 
-    std::cout << "First card value: " << getCardValue(deck[0]) << '\n';
+        std::cout << '\n';
+    }
+    else
+    {
+        shuffleDeck(deck);
+        std::cout << "Prepare table... Shuffle deck...\n";
+    }
+
+    std::cout << "Begin a game of blackjack!\n";
+
+    playBlackjack(deck);
     return 0;
 }
