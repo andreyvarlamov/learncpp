84b2e810aedbf39f58ad89399fcd7de6b3419792
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-19 08:34:05 -0800

    Tutorial > Overloading the arithmetic operators using friend functions

---
+++ 14-operator-overloading/14_02_overloading_arithmetic_op_friend_functions.md
@@ -0,0 +1,3 @@
+# 14.2 - Overloading the arithmetic operators using friend functions
+
+See `ex/01_minmax.cpp`
---
+++ 14-operator-overloading/ex/01_minmax.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class MinMax
+{
+private:
+    int m_min { };
+    int m_max { };
+
+public:
+    MinMax(int min, int max)
+        : m_min { min }, m_max { max }
+    {
+    }
+
+    int getMin() const { return m_min; }
+    int getMax() const { return m_max; }
+
+    friend MinMax operator+(const MinMax& m1, const MinMax& m2);
+    friend MinMax operator+(const MinMax& m, int value);
+    friend MinMax operator+(int value, const MinMax& m);
+};
+
+MinMax operator+(const MinMax& m1, const MinMax& m2)
+{
+    int min { m1.m_min < m2.m_min ? m1.m_min : m2.m_min };
+    int max { m1.m_max > m2.m_max ? m1.m_max : m2.m_max };
+
+    return { min, max };
+}
+
+MinMax operator+(const MinMax& m, int value)
+{
+    int min { m.m_min < value ? m.m_min : value };
+    int max { m.m_max > value ? m.m_max : value };
+
+    return { min, max };
+}
+
+MinMax operator+(int value, const MinMax& m)
+{
+    // call operator+(MinMax, int)
+    return { m + value };
+}
+
+int main()
+{
+    MinMax m1 { 10, 15 };
+    MinMax m2 { 8, 11 };
+    MinMax m3 { 3, 12 };
+
+    // MinMax mFinal = (((m1 + m2) + 5) + 8) + m3) + 16)
+    MinMax mFinal { m1 + m2 + 5 + 8 + m3 + 16 };
+
+    std::cout << "Result: (" << mFinal.getMin() << ", " << mFinal.getMax() << ")\n";
+
+    return 0;
+}
